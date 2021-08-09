3c599430512c6910b9e92b9dac7630ed292fce87
Michał Tracewicz <m.tracewicz@gmail.com>
2021-08-09 21:58:07 +0200

    Changed speed to be more "life-like"

--- src/planetDefinitions.js
+++ src/planetDefinitions.js
@@ -1,10 +1,10 @@
 const planets = [
-  { size: 1, distance: 1, speed: 0.005, color: 0xf03a47 },
-  { size: 1, distance: 2, speed: 0.01, color: 0xffffff },
-  { size: 1, distance: 3, speed: 0.015, color: 0x2e2e2e },
+  { size: 1, distance: 1, speed: 0.035, color: 0xf03a47 },
+  { size: 1, distance: 2, speed: 0.03, color: 0xffffff },
+  { size: 1, distance: 3, speed: 0.025, color: 0x2e2e2e },
   { size: 1, distance: 4, speed: 0.02, color: 0xff0000 },
-  { size: 1, distance: 5, speed: 0.025, color: 0x00ff00 },
-  { size: 1, distance: 6, speed: 0.03, color: 0x0000ff },
-  { size: 1, distance: 7, speed: 0.035, color: 0xf0f0f0 },
+  { size: 1, distance: 5, speed: 0.015, color: 0x00ff00 },
+  { size: 1, distance: 6, speed: 0.01, color: 0x0000ff },
+  { size: 1, distance: 7, speed: 0.005, color: 0xf0f0f0 },
 ];
 export default { planets };
